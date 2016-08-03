972a9fa87ce1d0f4f8368549eec9b32dbdab8c74
TuyueChen <tchen@mybillingtree.com>
2016-08-02 23:01:26 -0700

    add some basic operation to the program

--- TfsManager.py
+++ TfsManager.py
@@ -1,5 +1,4 @@
 import urllib2, base64, sys, json
-
 class CommandException(Exception):
     pass
 
@@ -15,18 +14,20 @@ class Argument:
     token = ""
     mode = Mode.quick
     def __readConfig(self):        
-        f = open('config.js', 'r');
+        f = open('config.js', 'r')
         config = json.loads(f.read())
         if 'baseUrl' not in config or 'user' not in config\
             or 'token' not in config or 'projId' not in config:
-
             raise CommandException("Please provide the sufficient access info")
+        self.assignedTo = "Tuyue Chen"
+        self.sort = 'id'
         self.user = config['user']
         self.token = config['token']
         self.baseUrl = config['baseUrl']
+        self.projId = config['projId']
+        self.iteration = config['iteration']
     def __readStr (self,args, i):
-        args[i]
-        return i+1
+        return i+1, args[i+1]
     def __readNums (self, args, i):
         nums = [];
         for index in range(i+1, len(args)):
@@ -47,12 +48,45 @@ class Argument:
                  i, self.tickets = self.__readNums(args,i)
                  break;
              if (args[i] == '-l'):
-                 self.mode = Mode.quick        
+                 self.mode = Mode.quick 
+             if (args[i] == '-a'):
+                 i, self.assignedTo = self.__readStr(args,i)
+             if (args[i] == '-s'):
+                 i, self.sort = self.__readStr(args,i)
              i+=1
                  
 
 try:
     x = Argument(sys.argv)
-    if x.mode == Mode.t
+    if x.mode == Mode.quick:
+        ##Step1: get the query result
+        url = "https://%s/DefaultCollection/%s/_apis/wit/wiql?api-version=1.0"%(x.baseUrl,x.projId)
+        query  = "SELECT [System.Id], [System.Title],[System.State]   FROM WorkItems    WHERE [System.AssignedTo] = '%s' AND [System.IterationPath] Under '%s'"%(x.assignedTo, x.iteration)
+        data = json.dumps({'query':query})
+        request = urllib2.Request(url,data);
+        request.add_header("Content-type","application/json")
+        base64string = base64.encodestring('%s:%s'%(x.user,x.token)).replace('\n','')
+        request.add_header("Authorization","Basic %s" % base64string)
+        httpResult = urllib2.urlopen(request)
+        responseData = json.loads(httpResult.read())
+        ##Step2: get the item detail from the query result
+        id_list = []
+        for item in responseData["workItems"]:
+            id_list.append(str(item["id"]))
+        idStr = 'ids='+','.join(id_list)
+        fieldStr = 'fields=System.Id,System.Title,System.State'
+        apiStr = 'api-version=1.0'
+        url2 = "https://%s/DefaultCollection/_apis/wit/WorkItems?%s&%s&%s"%(x.baseUrl,idStr,fieldStr,apiStr)
+        request2 = urllib2.Request(url2);
+        request2.add_header("Authorization", "Basic %s" % base64string)
+        result = urllib2.urlopen(request2)
+        responseData = json.loads(result.read())
+        list = []
+        for item in responseData["value"]:
+        	list.append({'id':item["fields"]["System.Id"], 'state': item["fields"]["System.State"], 'title':item["fields"]["System.Title"]})
+        from operator import itemgetter
+        newlist = sorted(list, key=itemgetter(x.sort)) 
+        for item in newlist:
+        	print '%s\t %s\t%s' % (item["id"], item["state"].encode('utf-8').ljust(12), item["title"].encode('utf-8'))
 except CommandException as e:
     print str(e)
--- config.js
+++ config.js
@@ -2,5 +2,6 @@
 	"user": "then",
 	"token":"",
 	"baseUrl":"",
-	"projId": "1f156dfc-c856-4cc3-874a-4796119edc35"
+	"projId": "1f156dfc-c856-4cc3-874a-4796119edc35",
+	"iteration":""
 }
